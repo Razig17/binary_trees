8867fb87da41ea891d1f8848fc1f1ff553a412aa
Razig17 <abdelrazig.sharif1@gmail.com>
2024-04-11 03:36:31 +0300

    Is leaf

--- binary_trees.h
+++ binary_trees.h
@@ -58,7 +58,15 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
  * @tree: Pointer to the root node of the tree to delete
  *
  */
-
 void binary_tree_delete(binary_tree_t *tree);
+
+/**
+ * binary_tree_is_leaf - Checks if a node is a leaf
+ *
+ * @node: Pointer to the node to check
+ *
+ * Return: return 1 if node is a leaf, otherwise 0
+ */
+int binary_tree_is_leaf(const binary_tree_t *node);
 
 #endif /* _BINARY_TREES_H_ */
