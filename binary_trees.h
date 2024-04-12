832c3ef7b8503d0c8c1cbe662d4a3aea3407abe8
Razig17 <abdelrazig.sharif1@gmail.com>
2024-04-12 03:57:41 +0300

    Leaves

--- binary_trees.h
+++ binary_trees.h
@@ -179,4 +179,12 @@ int binary_tree_is_full(const binary_tree_t *tree);
  */
 int is_full(const binary_tree_t *tree);
 
+/**
+ * binary_tree_is_perfect - Checks if a binary tree is perfect
+ * @tree: A pointer to the root node of the tree to check
+ *
+ * Return: 1 if the tree is perfect, 0 otherwise
+ */
+int binary_tree_is_perfect(const binary_tree_t *tree);
+
 #endif /* _BINARY_TREES_H_ */
