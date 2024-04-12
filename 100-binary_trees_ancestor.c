0a1f83366ea701bf11c8d5fcff102938cc79dd7e
Razig17 <abdelrazig.sharif1@gmail.com>
2024-04-12 04:38:07 +0300

    Common Ancestor

---
+++ 100-binary_trees_ancestor.c
@@ -0,0 +1,34 @@
+#include "binary_trees.h"
+/**
+ * binary_trees_ancestor - the lowest common ancestor of two nodes
+ *
+ * @first: pointer to the first node
+ * @second: pointer to the second node
+ * Return: return a pointer to the lowest common ancestor , otherwise NULL
+ */
+
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first,
+		const binary_tree_t *second)
+{
+	binary_tree_t *tmp1, *tmp2;
+
+	if (first == NULL || second == NULL)
+		return (NULL);
+	tmp1 = first->parent;
+	tmp2 = second->parent;
+	while (tmp1)
+	{
+		while (tmp2)
+		{
+			if (tmp2 == tmp1)
+				return (tmp1);
+			tmp2 = tmp2->parent;
+		}
+		tmp1 = tmp1->parent;
+		if (tmp1 == NULL)
+			break;
+		tmp2 = second->parent;
+	}
+
+	return (NULL);
+}
--- binary_trees.h
+++ binary_trees.h
@@ -205,4 +205,13 @@ binary_tree_t *binary_tree_sibling(binary_tree_t *node);
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node);
 
+/**
+ * binary_trees_ancestor - the lowest common ancestor of two nodes
+ *
+ * @first: pointer to the first node
+ * @second: pointer to the second node
+ * Return: return a pointer to the lowest common ancestor , otherwise NULL
+ */
+binary_tree_t *binary_trees_ancestor(const binary_tree_t *first, const binary_tree_t *second);
+
 #endif /* _BINARY_TREES_H_ */
